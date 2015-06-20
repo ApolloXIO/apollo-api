3376880a9105f387b3ec0dc8355863c932ab1d03
Ayush <ayush29f@uw.edu>
2015-06-20 16:25:46 +0530

    added 404

--- server.js
+++ server.js
@@ -29,6 +29,17 @@ app.use(passport.session()); // persistent login sessions
 // ROUTES
 // ======
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+
+
+
 app.get('/', function (req, res) {
 	res.send("Hello World!");
 });
