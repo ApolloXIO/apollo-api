8b930e40ce8b0d4d8178237608f85f7fb0200b63
Ayush <ayush29f@uw.edu>
2015-06-20 16:07:11 +0530

    Hello World Setup

---
+++ server.js
@@ -0,0 +1,42 @@
+// ===========
+// BASIC SETUP
+// ==========
+
+var express  = require('express');
+var app 	 = express();
+var port     = process.env.PORT || 8080;
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var mongoose = require('mongoose');
+var passport = require('passport');
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+// ======
+// ROUTES
+// ======
+
+app.get('/', function (req, res) {
+	res.send("Hello World!");
+});
+
+
+
+// ===================
+// Run The Express App
+// ===================
+app.listen(port);
+console.log('The Hacking Begins at port ' + port);
